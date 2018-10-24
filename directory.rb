62dc2825757c90ec24bbdae04e7e3b6fc46e66a3
Jackie <kie.dunne@gmail.com>
2018-10-24 01:22:10 +0100

    Added nationality to directory.rb and added typos.rb

--- directory.rb
+++ directory.rb
@@ -1,31 +1,42 @@
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
   students = []
-  name = gets.chomp
-
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while true do
+    puts "Please enter the names of a student"
+    puts "To finish, just type stop"
     name = gets.chomp
+    break if name == "stop"
+
+    puts "Please enter the cohort of the student"
+    puts "To finish, just type stop"
+    cohort = gets.chomp
+    break if cohort == "stop"
+
+    puts "Please enter the nationality of the student"
+    puts "To finish, just type stop"
+    nationality = gets.chomp
+    break if nationality == "stop"
+
+    students << {name: name, cohort: cohort, nationality: nationality}
+    puts "Now we have #{students.count} students. Name of next student?"
+
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The Students of Villains Academy".center(100)
+  puts "--------------------------".center(100)
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (Cohort: #{student[:cohort]} ) (Nationality: #{student[:nationality]} )".center(100)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+  puts "Overall, we have #{names.count} great students.".center(100)
 end
 
 students = input_students
