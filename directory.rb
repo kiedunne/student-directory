3ef40da9cc366515604cd1cbc0a47fceb7a0aede
Jackie <kie.dunne@gmail.com>
2018-10-24 02:10:52 +0100

    Save students in file

--- directory.rb
+++ directory.rb
@@ -35,12 +35,13 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print
+  print_students_list
   print_footer
 end
 
@@ -50,6 +51,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -68,8 +71,23 @@ def print_students_list
   end
 end
 #
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students.".center(100)
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student.".center(100)
+  else
+  puts "Overall, we have #{@students.count} great students.".center(100)
+  end
+end
+ 
+def save_students
+  file = File.open('students.csv', 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:nationality]]
+    csv_line = student_data.join(',')
+    file.puts csv_line
+  end
+  file.close
+  puts "students.csv created!"
 end
 
 interactive_menu
